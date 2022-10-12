0051e3dc02ffd83436e2c473fe429ee3592a670c
mayur <mdave3927@gmail.com>
2022-10-12 01:34:53 +0100

    ✅ calculate car assembley metrics

--- go/cars-assemble/cars_assemble.go
+++ go/cars-assemble/cars_assemble.go
@@ -1,18 +1,21 @@
 package cars
 
+const costOfTenCars = 95_000
+const costOfOneCar = 10_000
+
 // CalculateWorkingCarsPerHour calculates how many working cars are
 // produced by the assembly line every hour.
 func CalculateWorkingCarsPerHour(productionRate int, successRate float64) float64 {
-	panic("CalculateWorkingCarsPerHour not implemented")
+	return (float64(productionRate) * successRate)/100
 }
 
 // CalculateWorkingCarsPerMinute calculates how many working cars are
 // produced by the assembly line every minute.
 func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) int {
-	panic("CalculateWorkingCarsPerMinute not implemented")
+	return int(CalculateWorkingCarsPerHour(productionRate, successRate)) / 60
 }
 
 // CalculateCost works out the cost of producing the given number of cars.
 func CalculateCost(carsCount int) uint {
-	panic("CalculateCost not implemented")
+	return (uint(carsCount % 10) * costOfOneCar) + (uint(carsCount / 10) * costOfTenCars)
 }
