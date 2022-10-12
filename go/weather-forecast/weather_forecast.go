57ec6d5780471677124b3821ba5dead9bcb39b33
mayur <mdave3927@gmail.com>
2022-10-12 01:19:20 +0100

    💡✅ add comments, useful for godoc

--- go/weather-forecast/weather_forecast.go
+++ go/weather-forecast/weather_forecast.go
@@ -1,8 +1,13 @@
+// Package weather provides tools to tell us about the weather.
 package weather
 
+// CurrentCondition represents the current condition.
 var CurrentCondition string
+// CurrentLocation represents the current location.
 var CurrentLocation string
 
+
+// Forecast returns a formatted sentence describing the weather forecast.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
