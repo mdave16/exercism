b03171eb1d67621230b60832cce0e74c514eb8cd
mayur <mdave3927@gmail.com>
2022-10-24 02:55:31 +0100

    ✅ use different packages e.g. math/rand and time

--- go/animal-magic/animal_magic.go
+++ go/animal-magic/animal_magic.go
@@ -1,21 +1,34 @@
 package chance
 
+import (
+	"math/rand"
+	"time"
+)
+
 // SeedWithTime seeds math/rand with the current computer time.
 func SeedWithTime() {
-	panic("Please implement the SeedWithTime function")
+	rand.Seed(time.Now().UnixNano())
 }
 
 // RollADie returns a random int d with 1 <= d <= 20.
 func RollADie() int {
-	panic("Please implement the RollADie function")
+	return rand.Intn(20) + 1
 }
 
 // GenerateWandEnergy returns a random float64 f with 0.0 <= f < 12.0.
 func GenerateWandEnergy() float64 {
-	panic("Please implement the GenerateWandEnergy function")
+	return rand.Float64() * 12
+}
+
+func swap(arr []string) func(i, j int) {
+	return func(i, j int) {
+		arr[i], arr[j] = arr[j], arr[i]
+	}
 }
 
 // ShuffleAnimals returns a slice with all eight animal strings in random order.
 func ShuffleAnimals() []string {
-	panic("Please implement the ShuffleAnimals function")
+	animals := []string{"ant", "beaver", "cat", "dog", "elephant", "fox", "giraffe", "hedgehog"}
+	rand.Shuffle(len(animals), swap(animals))
+	return animals
 }
