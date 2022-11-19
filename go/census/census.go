c9a03f91d27a1078ace81f32841c5071e00bd74a
mayur <mdave3927@gmail.com>
2022-11-19 00:13:28 +0000

    ♻️ simplify solution

--- go/census/census.go
+++ go/census/census.go
@@ -19,14 +19,12 @@ func NewResident(name string, age int, address map[string]string) *Resident {
 
 // HasRequiredInfo determines if a given resident has all of the required information.
 func (r *Resident) HasRequiredInfo() bool {
-	return r.Name != "" && r.Address != nil && r.Address["street"] != ""
+	return r.Name != "" && r.Address["street"] != ""
 }
 
 // Delete deletes a resident's information.
 func (r *Resident) Delete() {
-	r.Name = ""
-	r.Age = 0
-	r.Address = nil
+	*r = Resident{}
 }
 
 // Count counts all residents that have provided the required information.
@@ -34,7 +32,7 @@ func Count(residents []*Resident) int {
 	sum := 0
 	for _, v := range residents {
 		if v.HasRequiredInfo() {
-			sum += 1
+			sum++
 		}
 	}
 	return sum
