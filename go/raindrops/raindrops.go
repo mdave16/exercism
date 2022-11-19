5b0c7997d8b1df44bc29d02a4cc9db2e8d9ce256
mayur <mdave3927@gmail.com>
2022-11-19 04:10:48 +0000

    ✅ if elses to solve fizzbuzz

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -1,5 +1,19 @@
 package raindrops
 
-func Convert(number int) string {
-	panic("Please implement the Convert function")
+import "fmt"
+
+func Convert(number int) (sound string) {
+	if number%3 == 0 {
+		sound += "Pling"
+	}
+	if number%5 == 0 {
+		sound += "Plang"
+	}
+	if number%7 == 0 {
+		sound += "Plong"
+	}
+	if len(sound) == 0 {
+		sound += fmt.Sprint(number)
+	}
+	return
 }
