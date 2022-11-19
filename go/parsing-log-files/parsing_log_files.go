1436ef3f83c2cfba7b66b3dbd71053dbf02d733b
mayur <mdave3927@gmail.com>
2022-11-19 00:43:07 +0000

    ✅ use regexes to parse log files

--- go/parsing-log-files/parsing_log_files.go
+++ go/parsing-log-files/parsing_log_files.go
@@ -1,21 +1,47 @@
 package parsinglogfiles
 
+import (
+	"fmt"
+	"regexp"
+)
+
+// IsValidLine determines if text starts with valid log descriptions.
 func IsValidLine(text string) bool {
-	panic("Please implement the IsValidLine function")
+	var re = regexp.MustCompile(`^\[(TRC|DBG|INF|WRN|ERR|FTL)\].*`)
+	return re.MatchString(text)
 }
 
+// SplitLogLine by separators used by the new team.
 func SplitLogLine(text string) []string {
-	panic("Please implement the SplitLogLine function")
+	var re = regexp.MustCompile(`<[~*=-]*>`)
+	return re.Split(text, -1)
 }
 
+// CountQuotedPasswords counts lines that reference "password" in quotes.
 func CountQuotedPasswords(lines []string) int {
-	panic("Please implement the CountQuotedPasswords function")
+	var re = regexp.MustCompile(`".*[pP][aA][sS][sS][wW][oO][rR][dD].*"`)
+	sum := 0
+	for _, log := range lines {
+		if re.MatchString(log) {
+			sum++
+		}
+	}
+	return sum
 }
 
+// RemoveEndOfLineText removes end-of-line<lineNumber> used by some teams.
 func RemoveEndOfLineText(text string) string {
-	panic("Please implement the RemoveEndOfLineText function")
+	var re = regexp.MustCompile(`end-of-line\d+`)
+	return re.ReplaceAllString(text, "")
 }
 
+// TagWithUserName will tag lines referencing users to the users mentioned.
 func TagWithUserName(lines []string) []string {
-	panic("Please implement the TagWithUserName function")
+	var re = regexp.MustCompile(`User\s+([^\s]+)`)
+	for index, log := range lines {
+		if match := re.FindStringSubmatch(log); match != nil {
+			lines[index] = fmt.Sprintf("[USR] %s %s", match[1], log)
+		}
+	}
+	return lines
 }
