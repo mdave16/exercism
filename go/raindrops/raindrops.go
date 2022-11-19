e4f64d63dbf6e7572b64b010e6180120d2243237
mayur <mdave3927@gmail.com>
2022-11-19 04:19:14 +0000

    ♻️ use logic as data

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -2,15 +2,28 @@ package raindrops
 
 import "fmt"
 
+var rules = []struct {
+	divides int
+	sound   string
+}{{
+	divides: 3,
+	sound:   "Pling",
+},
+	{
+		divides: 5,
+		sound:   "Plang",
+	},
+	{
+		divides: 7,
+		sound:   "Plong",
+	},
+}
+
 func Convert(number int) (sound string) {
-	if number%3 == 0 {
-		sound += "Pling"
-	}
-	if number%5 == 0 {
-		sound += "Plang"
-	}
-	if number%7 == 0 {
-		sound += "Plong"
+	for _, rule := range rules {
+		if number%rule.divides == 0 {
+			sound += rule.sound
+		}
 	}
 	if len(sound) == 0 {
 		sound += fmt.Sprint(number)
