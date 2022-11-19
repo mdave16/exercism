10fac5be904c4fe4d3bd78a2aa06a3750faa8395
mayur <mdave3927@gmail.com>
2022-11-19 00:11:26 +0000

    ✅ use nil values

--- go/census/census.go
+++ go/census/census.go
@@ -10,20 +10,32 @@ type Resident struct {
 
 // NewResident registers a new resident in this city.
 func NewResident(name string, age int, address map[string]string) *Resident {
-	panic("Please implement NewResident.")
+	return &Resident{
+		Name:    name,
+		Age:     age,
+		Address: address,
+	}
 }
 
 // HasRequiredInfo determines if a given resident has all of the required information.
 func (r *Resident) HasRequiredInfo() bool {
-	panic("Please implement HasRequiredInfo.")
+	return r.Name != "" && r.Address != nil && r.Address["street"] != ""
 }
 
 // Delete deletes a resident's information.
 func (r *Resident) Delete() {
-	panic("Please implement Delete.")
+	r.Name = ""
+	r.Age = 0
+	r.Address = nil
 }
 
 // Count counts all residents that have provided the required information.
 func Count(residents []*Resident) int {
-	panic("Please implement Count.")
+	sum := 0
+	for _, v := range residents {
+		if v.HasRequiredInfo() {
+			sum += 1
+		}
+	}
+	return sum
 }
