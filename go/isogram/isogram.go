5ce10aa4d1868ca11ef63143e0ab680e18af5b4d
mayur <mdave3927@gmail.com>
2022-11-20 01:13:50 +0000

    ✅ check if words are isograms

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -1,5 +1,21 @@
 package isogram
 
+import "strings"
+
+func lettersOnly(word string) string {
+	return strings.ToUpper(strings.ReplaceAll(
+		strings.ReplaceAll(word, " ", ""), "-", ""))
+}
+
 func IsIsogram(word string) bool {
-	panic("Please implement the IsIsogram function")
+	var seen = map[rune]bool{}
+	for _, r := range lettersOnly(word) {
+		_, isSeen := seen[r]
+		if isSeen {
+			return false
+		} else {
+			seen[r] = true
+		}
+	}
+	return true
 }
