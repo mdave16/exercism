d7d36708418f3e662d324ef707c5d3d17dbb0cea
mayur <mdave3927@gmail.com>
2022-11-22 16:59:39 +0000

    ✅ convert rna to proteins

--- go/protein-translation/protein_translation.go
+++ go/protein-translation/protein_translation.go
@@ -1,9 +1,44 @@
 package protein
 
+import "errors"
+
+var ErrStop = errors.New("received stop codon")
+var ErrInvalidBase = errors.New("invalid base")
+
 func FromRNA(rna string) ([]string, error) {
-	panic("Please implement the FromRNA function")
+	proteins := []string{}
+	for i := 0; i < len(rna); i += 3 {
+		protein, err := FromCodon(rna[i : i+3])
+		if err == ErrStop {
+			break
+		}
+		if err == ErrInvalidBase {
+			return nil, ErrInvalidBase
+		}
+
+		proteins = append(proteins, protein)
+	}
+	return proteins, nil
 }
 
 func FromCodon(codon string) (string, error) {
-	panic("Please implement the FromCodon function")
+	switch codon {
+	case "AUG":
+		return "Methionine", nil
+	case "UUU", "UUC":
+		return "Phenylalanine", nil
+	case "UUA", "UUG":
+		return "Leucine", nil
+	case "UCU", "UCC", "UCA", "UCG":
+		return "Serine", nil
+	case "UAU", "UAC":
+		return "Tyrosine", nil
+	case "UGU", "UGC":
+		return "Cysteine", nil
+	case "UGG":
+		return "Tryptophan", nil
+	case "UAA", "UAG", "UGA":
+		return "", ErrStop
+	}
+	return "", ErrInvalidBase
 }
