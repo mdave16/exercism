012111071be07c1579bc6f269e6241f29af59ddb
mayur <mdave3927@gmail.com>
2022-11-22 01:44:38 +0000

    ✅ build a proverb

--- go/proverb/proverb.go
+++ go/proverb/proverb.go
@@ -1,15 +1,16 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package proverb should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
 package proverb
 
-// Proverb should have a comment documenting it.
-func Proverb(rhyme []string) []string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	panic("Please implement the Proverb function")
+import "fmt"
+
+func Proverb(rhyme []string) (proverb []string) {
+	var line string
+	for i := range rhyme {
+		if i+1 < len(rhyme) {
+			line = fmt.Sprintf("For want of a %v the %v was lost.", rhyme[i], rhyme[i+1])
+		} else {
+			line = fmt.Sprintf("And all for the want of a %v.", rhyme[0])
+		}
+		proverb = append(proverb, line)
+	}
+	return
 }
