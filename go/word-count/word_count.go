e460a4412d4c04252ca2a6290ac87df7202ddbd8
mayur <mdave3927@gmail.com>
2022-11-22 20:49:14 +0000

    ✅ calculate word frequency

--- go/word-count/word_count.go
+++ go/word-count/word_count.go
@@ -1,7 +1,20 @@
 package wordcount
 
+import (
+	"regexp"
+	"strings"
+)
+
 type Frequency map[string]int
 
 func WordCount(phrase string) Frequency {
-	panic("Please implement the WordCount function")
+	re := regexp.MustCompile(`([a-z0-9]+')?[a-z0-9]+`)
+	freq := make(map[string]int)
+	for _, word := range re.FindAll([]byte(strings.ToLower(phrase)), -1) {
+		if len(word) == 0 {
+			continue
+		}
+		freq[string(word)]++
+	}
+	return freq
 }
