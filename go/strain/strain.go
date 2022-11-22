f8217e085239afeeb129d2427dd60cbb16baf317
mayur <mdave3927@gmail.com>
2022-11-22 16:27:08 +0000

    ✅ implement select and reject

--- go/strain/strain.go
+++ go/strain/strain.go
@@ -4,18 +4,33 @@ type Ints []int
 type Lists [][]int
 type Strings []string
 
-func (i Ints) Keep(filter func(int) bool) Ints {
-	panic("Please implement the Keep function")
+func (i Ints) Keep(filter func(int) bool) (out Ints) {
+	for _, v := range i {
+		if filter(v) {
+			out = append(out, v)
+		}
+	}
+	return
 }
 
 func (i Ints) Discard(filter func(int) bool) Ints {
-	panic("Please implement the Discard function")
+	return i.Keep(func(n int) bool { return !filter(n) })
 }
 
-func (l Lists) Keep(filter func([]int) bool) Lists {
-	panic("Please implement the Keep function")
+func (l Lists) Keep(filter func([]int) bool) (out Lists) {
+	for _, v := range l {
+		if filter(v) {
+			out = append(out, v)
+		}
+	}
+	return
 }
 
-func (s Strings) Keep(filter func(string) bool) Strings {
-	panic("Please implement the Keep function")
+func (s Strings) Keep(filter func(string) bool) (out Strings) {
+	for _, v := range s {
+		if filter(v) {
+			out = append(out, v)
+		}
+	}
+	return
 }
