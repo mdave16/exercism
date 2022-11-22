1d314529d29b484f047e9cc53173888025ee6d83
mayur <mdave3927@gmail.com>
2022-11-22 00:17:59 +0000

    ✅ tell the time

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -1,19 +1,31 @@
 package clock
 
-// Define the Clock type here.
+import "fmt"
+
+type Clock struct {
+	minute int
+}
+
+func postiveMod(b int) func(int) int {
+	return func(a int) int {
+		return ((a % b) + b) % b
+	}
+}
+
+var inDay = postiveMod(24 * 60)
 
 func New(h, m int) Clock {
-	panic("Please implement the New function")
+	return Clock{0}.Add(inDay(h*60 + m))
 }
 
 func (c Clock) Add(m int) Clock {
-	panic("Please implement the Add function")
+	return Clock{inDay(c.minute + m)}
 }
 
 func (c Clock) Subtract(m int) Clock {
-	panic("Please implement the Subtract function")
+	return Clock{inDay(c.minute - m)}
 }
 
 func (c Clock) String() string {
-	panic("Please implement the String function")
+	return fmt.Sprintf("%02d:%02d", c.minute/60, c.minute%60)
 }
