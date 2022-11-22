86c6203aa483abb1c3b353257f7b5cf07aabe3c4
mayur <mdave3927@gmail.com>
2022-11-22 01:06:29 +0000

    ♻️ collapse conditions

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -3,14 +3,5 @@ package leap
 
 // IsLeapYear calculates if a given year is a Leap Year.
 func IsLeapYear(year int) bool {
-	if year%400 == 0 {
-		return true
-	}
-	if year%100 == 0 {
-		return false
-	}
-	if year%4 == 0 {
-		return true
-	}
-	return false
+	return year%400 == 0 || (year%100 != 0 && year%4 == 0)
 }
