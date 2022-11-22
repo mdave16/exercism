f236da2468e53510cf6df6f953869b8c5bcc0776
mayur <mdave3927@gmail.com>
2022-11-22 01:02:58 +0000

    ♻️ flatten if else conditions

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -3,13 +3,13 @@ package leap
 
 // IsLeapYear calculates if a given year is a Leap Year.
 func IsLeapYear(year int) bool {
+	if year%400 == 0 {
+		return true
+	}
+	if year%100 == 0 {
+		return false
+	}
 	if year%4 == 0 {
-		if year%100 == 0 {
-			if year%400 == 0 {
-				return true
-			}
-			return false
-		}
 		return true
 	}
 	return false
