b8dc543d2c1668799f197b53bbc9b80e1e83fac5
mayur <mdave3927@gmail.com>
2022-11-22 17:31:10 +0000

    ✅ reverse string

--- go/reverse-string/reverse_string.go
+++ go/reverse-string/reverse_string.go
@@ -1,5 +1,11 @@
 package reverse
 
 func Reverse(input string) string {
-	panic("Please implement the Reverse function")
+	runes := []rune(input)
+	numRunes := len(runes)
+	output := make([]rune, numRunes)
+	for i, l := range runes {
+		output[numRunes-i-1] = l
+	}
+	return string(output)
 }
