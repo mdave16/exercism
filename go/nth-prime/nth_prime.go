cc507158fee17c08cf6cd4062d36a67184d10ef2
mayur <mdave3927@gmail.com>
2022-11-23 01:02:13 +0000

    ✅ calculate nth prime

--- go/nth-prime/nth_prime.go
+++ go/nth-prime/nth_prime.go
@@ -1,6 +1,29 @@
 package prime
 
+import "errors"
+
+var primes []int = []int{2}
+
 // Nth returns the nth prime number. An error must be returned if the nth prime number can't be calculated ('n' is equal or less than zero)
 func Nth(n int) (int, error) {
-	panic("Please implement the Nth function")
+	if n <= 0 {
+		return 0, errors.New("there is no zeroth/negative prime")
+	}
+	for len(primes) < n {
+		lastPrime := primes[len(primes)-1]
+		for i := (lastPrime + 1); i < lastPrime*2; i++ {
+			divisible := false
+			for _, p := range primes {
+				if i%p == 0 {
+					divisible = true
+					continue
+				}
+			}
+			if divisible {
+				continue
+			}
+			primes = append(primes, i)
+		}
+	}
+	return primes[n-1], nil
 }
