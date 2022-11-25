7957121b9db618ad6f547361e36a0db46b20d593
mayur <mdave3927@gmail.com>
2022-11-25 00:51:01 +0000

    ♻️ use adapter pattern

--- go/rotational-cipher/rotational_cipher.go
+++ go/rotational-cipher/rotational_cipher.go
@@ -1,14 +1,20 @@
 package rotationalcipher
 
-
+func shiftFrom(key int, from rune) func(rune) rune {
+	return func(l rune) rune {
+		return rune((int(l-from)+key)%26) + from
+	}
+}
 
 func shiftFn(shiftKey int) func(rune) rune {
+	lowercaseShift := shiftFrom(shiftKey, 'a')
+	uppercaseShift := shiftFrom(shiftKey, 'A')
 	return func(l rune) rune {
-		if('a' <= l && l <= 'z') {
-			return (rune((int(l-'a') + shiftKey)%26) + 'a')
+		if 'a' <= l && l <= 'z' {
+			return lowercaseShift(l)
 		}
-		if('A' <= l && l <= 'Z') {
-			return (rune((int(l-'A') + shiftKey)%26) + 'A')
+		if 'A' <= l && l <= 'Z' {
+			return uppercaseShift(l)
 		}
 		return l
 	}
@@ -17,7 +23,7 @@ func shiftFn(shiftKey int) func(rune) rune {
 func RotationalCipher(plain string, shiftKey int) string {
 	shift := shiftFn(shiftKey)
 	secret := []rune{}
-	for _, v:= range plain {
+	for _, v := range plain {
 		secret = append(secret, shift(v))
 	}
 	return string(secret)
