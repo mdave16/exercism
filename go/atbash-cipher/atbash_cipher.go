c15f17b0083b93f70feb63edaf624531088c2259
mayur <mdave3927@gmail.com>
2022-11-25 01:15:18 +0000

    💩 ✅ implement atbash cipher, badly

--- go/atbash-cipher/atbash_cipher.go
+++ go/atbash-cipher/atbash_cipher.go
@@ -1,5 +1,32 @@
 package atbash
 
-func Atbash(s string) string {
-	panic("Please implement the Atbash function")
+func Atbash(plain string) string {
+	secret := []rune{}
+	c := 0
+	for _, v := range plain {
+		if '0' <= v && v <= '9' {
+			if c == 5 {
+				secret = append(secret, ' ')
+				c = 0
+			}
+			secret = append(secret, v)
+			c++
+		} else if 'a' <= v && v <= 'z' {
+			if c == 5 {
+				secret = append(secret, ' ')
+				c = 0
+			}
+			secret = append(secret, rune('a'+'a'+25-int(v)))
+			c++
+		} else if 'A' <= v && v <= 'Z' {
+			if c == 5 {
+				secret = append(secret, ' ')
+				c = 0
+			}
+			secret = append(secret, rune('A'+'a'+25-int(v)))
+			c++
+		}
+
+	}
+	return string(secret)
 }
