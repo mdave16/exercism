ad675d69212d7f809a3339b3eeeb158c08c50a2f
mayur <mdave3927@gmail.com>
2022-11-25 00:45:06 +0000

    ✅ implement rotational cipher

--- go/rotational-cipher/rotational_cipher.go
+++ go/rotational-cipher/rotational_cipher.go
@@ -1,5 +1,24 @@
 package rotationalcipher
 
+
+
+func shiftFn(shiftKey int) func(rune) rune {
+	return func(l rune) rune {
+		if('a' <= l && l <= 'z') {
+			return (rune((int(l-'a') + shiftKey)%26) + 'a')
+		}
+		if('A' <= l && l <= 'Z') {
+			return (rune((int(l-'A') + shiftKey)%26) + 'A')
+		}
+		return l
+	}
+}
+
 func RotationalCipher(plain string, shiftKey int) string {
-	panic("Please implement the RotationalCipher function")
+	shift := shiftFn(shiftKey)
+	secret := []rune{}
+	for _, v:= range plain {
+		secret = append(secret, shift(v))
+	}
+	return string(secret)
 }
